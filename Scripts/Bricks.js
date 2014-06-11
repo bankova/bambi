cef7528019d34df1bdd76705c10766824cd60d88
RossyB <rossy@compass-bg.net>
2014-06-11 09:08:41 +0300

    the work of Boris

--- Scripts/Bricks.js
+++ Scripts/Bricks.js
@@ -1,56 +1,89 @@
-﻿window.onload = function () {
-    var theCanvas = document.getElementById("the-canvas");
-    var canvasCtx = theCanvas.getContext("2d");
+﻿window.onload = function () {    
+    var bricksGame = game("the-canvas");    
+    bricksGame.startGame();    
+}
 
-    function buildBall(x, y) {
-        return {
-            x: x,
-            y: y
-        };
+var theCanvas;
+var canvasCtx;
+
+function game(canvasContainerId) {
+    var self = {};        
+
+    theCanvas = document.getElementById(canvasContainerId);
+    canvasCtx = theCanvas.getContext("2d");    
+
+    var theball = ball(),
+        bricks = [];
+
+    function drawGameField() {
+        canvasCtx.clearRect(0, 0, theCanvas.width, theCanvas.height);
+        theball.draw();
+        drawBricks(30, 50, 40, 20, 1); // Move me when drawBricks() is moved
+        requestAnimationFrame(drawGameField);
     }
 
-    var ball = {};
-    ball.x = Math.floor(theCanvas.width / 2);
-    ball.y = Math.floor(theCanvas.height / 2);
-    ball.radius = 10;
+    self.startGame = function () {        
+        requestAnimationFrame(drawGameField);
+    }
 
-    var down = +1;
-    var rigth = +1;
-	var speed = 10;
+    return self;
+}
 
-    function animationFrame() {
-        canvasCtx.clearRect(0, 0, theCanvas.width, theCanvas.height);
+function ball() {
+    var self = {},
+        radius = 10,
+        speed = 5,
+        right = +1,
+        down = +1;
 
-        ball.x += speed*rigth;
-        ball.y += speed*down;
+    self.x = Math.floor(theCanvas.width / 2);
+    self.y = Math.floor(theCanvas.height / 2);
 
-        if (rigth == +1 && ball.x == theCanvas.width-ball.radius) {
-            rigth = -1;
-            ball.x += speed*rigth;
+    self.draw = function () {
+        self.x += speed * right;
+        self.y += speed * down;
+
+        if (right == +1 && self.x == theCanvas.width - radius) {
+            right = -1;
+            self.x += speed * right;
         }
 
-        if (down == +1 && ball.y == theCanvas.height-ball.radius) {
+        if (down == +1 && self.y == theCanvas.height - radius) {
             down = -1;
-            ball.y += speed*down;
+            self.y += speed * down;
         }
 
-        if (rigth == -1 && ball.x == ball.radius) {
-            rigth = +1;
-            ball.x += speed*rigth;
+        if (right == -1 && self.x == radius) {
+            right = +1;
+            self.x += speed * right;
         }
 
-        if (down == -1 && ball.y == ball.radius) {
+        if (down == -1 && self.y == radius) {
             down = +1;
-            ball.y += speed*down;
+            self.y += speed * down;
         }
 
         canvasCtx.fillStyle = '#FF0000';
         canvasCtx.beginPath();
-        canvasCtx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
+        canvasCtx.arc(self.x, self.y, radius, 0, 2 * Math.PI);
         canvasCtx.fill();
-        requestAnimationFrame(animationFrame);
- 
     }
 
-    animationFrame();
+    return self;
+}
+
+// Everything below Needs to be somewhere else
+function drawBricks(startpointX, startpointY, widthBrick, heightBrick, linewidth) {
+    while (startpointX + widthBrick < theCanvas.width) {
+        canvasCtx.beginPath();
+        canvasCtx.rect(startpointX, startpointY, widthBrick, heightBrick);
+        canvasCtx.fillStyle = 'green';
+        canvasCtx.fill();
+        canvasCtx.lineWidth = linewidth;
+        canvasCtx.strokeStyle = 'black';
+        canvasCtx.stroke();
+
+        //update position
+        startpointX += widthBrick + 10;
+    }
 }
