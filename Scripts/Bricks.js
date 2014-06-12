d2d8ecab427ded068cdcbdb8023452181908fdff
bankova <kristina.bankova@abv.bg>
2014-06-12 21:58:07 +0300

    Logic for moving pad, collision pad - Boris mainly

--- Scripts/Bricks.js
+++ Scripts/Bricks.js
@@ -22,7 +22,7 @@ function game(canvasContainerId) {
         var startpointX = 20,
             startpointY = 30,
             brickHeight = 20,
-            brickWidth = 40;
+            brickWidth = 80;
 
         while (startpointX + brickWidth < theCanvas.width) {
             var newBrick = brick(startpointX, startpointY, brickWidth, brickHeight);            
@@ -40,33 +40,27 @@ function game(canvasContainerId) {
         }
     }
 
-    function drawPad() {
-        //setup
-        var padStartX = 250,
-            padStartY = 470,
-            padWidth = 100,
-            padHeight = 10;
-
-        gamePad = pad(padStartX, padStartY, padWidth, padHeight);
-        gamePad.draw();
-
-    }
-
     function drawGameField() {
         canvasCtx.clearRect(0, 0, theCanvas.width, theCanvas.height);
         theball.draw();
-        checkForColision();
-        drawBricks();
-        drawPad()
-        
-        if (bricks.length === 0) {
+        if (theball.HasHitBottom) {
             alert("Game over!");
+            return;
+        };
+        
+        drawBricks();
+
+        if (bricks.length === 0) {
+            alert("You win!"); // or draw ultra-mega cool animation :)
             return;
         }
 
+        checkForColision();       
+        gamePad.draw();        
         requestAnimationFrame(drawGameField);
     }
-
+               
+    //Brick and ball
     function checkForColision() {
         for (var i = 0; i < bricks.length; i++) {
             var aBrick = bricks[i];
@@ -78,12 +72,10 @@ function game(canvasContainerId) {
                 break;
             }
         }
-        var aPad = gamePad;
 
-        if (theball.y + theball.radius >= aPad.padStartY &&
-            aPad.padStartX <= theball.x + theball.radius &&
-                theball.x - theball.radius <= aPad.padStartX + aBrick.width
-            ) {
+        if (theball.y + theball.radius >= gamePad.padStartY &&
+            gamePad.padStartX <= theball.x - theball.radius &&
+                theball.x + theball.radius <= gamePad.padStartX + gamePad.padWidth) {
             theball.down *= -1;
         }
     }
@@ -97,44 +89,52 @@ function game(canvasContainerId) {
 
 function ball() {
     var self = {},        
-        speed = 5;
+        speed = 3;
 
     self.radius = 10;
     self.right = +1;
     self.down = +1;
+    self.HasHitBottom = false;
 
-    self.x = Math.floor(theCanvas.width / 2);
+    self.x = 15;//Math.floor(theCanvas.width / 2);
     self.y = Math.floor(theCanvas.height / 2);
 
     self.draw = function () {
         self.x += speed * self.right;
         self.y += speed * self.down;
 
-        if (self.right == +1 && self.x == theCanvas.width - self.radius) {
+        // right
+        if (self.right == +1 && self.x >= theCanvas.width - self.radius) {
             self.right = -1;
             self.x += speed * self.right;
-        }
+        } else
 
-        if (self.down == +1 && self.y == theCanvas.height - self.radius) {
-            self.down = -1;
-            self.y += speed * self.down;
-        }
+        // down
+        if (self.down == +1 && self.y >= theCanvas.height - self.radius) {
+            //self.down = -1;
+            //self.y += speed * self.down;
+            self.HasHitBottom = true;            
+        } else
 
-        if (self.right == -1 && self.x == self.radius) {
+        // left
+        if (self.right == -1 && self.x <= self.radius) {
             self.right = +1;
             self.x += speed * self.right;
-        }
+        } else
 
-        if (self.down == -1 && self.y == self.radius) {
+        // up
+        if (self.down == -1 && self.y <= self.radius) {
             self.down = +1;
             self.y += speed * self.down;
-        }
+        }        
 
-        canvasCtx.fillStyle = '#FF0000';
-        canvasCtx.beginPath();
-        canvasCtx.arc(self.x, self.y, self.radius, 0, 2 * Math.PI);
-        canvasCtx.fill();
-    }
+        if (!self.HasHitBottom) {
+            canvasCtx.fillStyle = '#FF0000';
+            canvasCtx.beginPath();
+            canvasCtx.arc(self.x, self.y, self.radius, 0, 2 * Math.PI);
+            canvasCtx.fill();
+        }        
+    }  
 
     return self;
 }
@@ -158,20 +158,45 @@ function brick(startpointX, startpointY, width, height) {
     return self;
 }
 
-function pad(padStartX, padStartY, padWidth, padHeight) {
-    var self = {};
+function pad() {
+    var self = {},
+        moveDistance=50;
 
-    self.padStartX = padStartX;
-    self.padStartY = padStartY;
-    self.padWidth = padWidth;
-    self.padHeight = padHeight;
+    self.padWidth = 300;
+    self.padHeight = 10;
+    self.padStartX = theCanvas.width/2-self.padWidth/2;
+    self.padStartY = theCanvas.height-self.padHeight*2;   
 
     self.draw = function () {
+    document.body.addEventListener("keydown", function (e) {
+        if (!e) {
+            e = window.event;
+        }
+
+        switch (e.keyCode) {
+        case 37:
+            moveLeft();
+            break;
+        case 39:
+            moveRight();
+            break;
+        }
+    });
+    self.draw = function (){
         canvasCtx.beginPath();
         canvasCtx.rect(self.padStartX, self.padStartY, self.padWidth, self.padHeight);
         canvasCtx.fill();
         canvasCtx.stroke();
+        }
     }
+
+    function moveLeft() {
+        if (self.padStartX - moveDistance/4 > 0) self.padStartX -= moveDistance;
+    }
+
+    function moveRight() {
+        if (self.padStartX + self.padWidth + moveDistance/4 < theCanvas.width) self.padStartX += moveDistance;
+    }
+
     return self;
 }
-
